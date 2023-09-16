b76bcbf354412574a9e4a8a14e8762933576ca71
Imran-Haider-47 <imranhaider0247@gmail.com>
2023-09-16 20:38:51 +0300

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row"> 
+        <div class="col-3" > 
+            <img src="https://instagram.fsaw2-3.fna.fbcdn.net/v/t51.2885-19/97566921_2973768799380412_5562195854791540736_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fsaw2-3.fna.fbcdn.net&_nc_cat=104&_nc_ohc=qVJyzAmVXCoAX_sEcR-&edm=ABmJApABAAAA&ccb=7-5&oh=00_AfBydAL5o5mn0hvZGhpg64PhvLigCysQIX-SDbNd1ZDI2A&oe=650B1C5F&_nc_sid=b41fef" alt="">
+        </div>
+        <div class="col-9"> </div>
+   </div>
+</div>
+@endsection
